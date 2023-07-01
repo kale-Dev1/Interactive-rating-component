c09960729fa30703b61e2f9c7ca16459a0e34893
kale-Dev1 <kamarakale4@gmail.com>
2023-07-01 01:09:05 -0500

    get selected rating from user

--- interactive-rating-component-main/script.js
+++ interactive-rating-component-main/script.js
@@ -2,18 +2,25 @@ const review_card = document.getElementById("review");
 const submit_btn = document.querySelector(".submit-btn");
 const thank_you = document.getElementById("thank-you");
 const ratings = document.getElementById("ratings");
+const chosen = document.querySelector(".chosen");
 
-submit_btn.addEventListener("click", function (e) {
-  e.preventDefault();
-  console.log("clicked");
-  review_card.style.display = "none";
-  thank_you.classList.toggle("hide");
-});
+let number = 0;
 
+// Get the chosen rating and toggle active class on buttons
 ratings.addEventListener("click", function (e) {
   e.preventDefault();
   [...e.target.parentNode.children].forEach((child) =>
     child.classList.remove("active")
   );
   e.target.classList.toggle("active");
+  number = e.target.textContent;
+});
+
+// Display the Thank you card
+submit_btn.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  chosen.textContent = number;
+  review_card.style.display = "none";
+  thank_you.classList.toggle("hide");
 });
